9ed301a5d3d8d15d194750b912e6925472695a91
Karem Mostafa <81931963+EngKarem@users.noreply.github.com>
2022-11-03 13:14:29 +0200

    Delete migrations directory

--- migrations/sqls/20220811120421-OrderDetails-table-up.sql
+++
@@ -1,7 +0,0 @@
-/* Replace with your SQL commands */
-CREATE TABLE OrderDetails(
- id SERIAL PRIMARY KEY,
- quantity integer,
- order_id bigint REFERENCES orders(id),
- product_id bigint REFERENCES products(id)
-);
